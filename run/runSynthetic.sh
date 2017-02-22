9890252bcb156fb20df03f22e5cb1101557ceabf
yjpark <yjpark@kdd.snu.ac.kr>
2017-02-22 16:46:13 +0900

    scripts for building synthetic data

--- run/runSynthetic.sh
+++ run/runSynthetic.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+nTokens=1000000
+nRecords=1000000
+nRules=200000
+nTokensInRule=30000
+avgRecLen=5
+avgLhsLen=2
+avgRhsLen=2
+skewZ=1
+ratio=0
+
+./setSynthetic.sh $nTokens $nRecords $nRules $nTokensInRule $avgRecLen $avgLhsLen $avgRhsLen $skewZ $ratio
 
