f1ece244e98f1b0a994a5741d72aa34c070b7b95
yjpark <yjpark@kdd.snu.ac.kr>
2017-03-02 15:44:20 +0900

    script

--- run/runAlgorithms.sh
+++ run/runAlgorithms.sh
@@ -41,43 +41,53 @@ fi
 #JoinNaive1
 if [[ $RUN_Naive1 == "True" ]];
 then
+	date
 	./joinNaive1.sh $inputfile_one $inputfile_two $rulefile $outputPath $dir $LIBS $project
 	echo java -Xmx8G -Xms4G -cp $LIBS mine.Naive1 $inputfile_one $inputfile_two $rulefile -1
 	{ time java -Xmx8G -Xms4G -cp $LIBS mine.Naive1 $inputfile_one $inputfile_two $rulefile -1 > $dir"/"logNaive1; }
+	date
 fi
 
 #JoinNaive2
 if [[ $RUN_Naive2 == "True" ]];
 then
+	date
 	./joinNaive2.sh $inputfile_one $inputfile_two $rulefile $outputPath $dir $LIBS $project
 	echo java -Xmx8G -Xms4G -cp $LIBS mine.Naive2 $inputfile_one $inputfile_two $rulefile
 	{ time java -Xmx8G -Xms4G -cp $LIBS mine.Naive2 $inputfile_one $inputfile_two $rulefile > $dir"/"logNaive2; }
+	date
 fi
 
 #SIJoin
 if [[ $RUN_SIJoin == "True" ]];
 then
+	date
 	./joinSI.sh $inputfile_one $inputfile_two $rulefile $outputPath $dir $LIBS $project
 	echo java -Xmx8G -Xms4G -cp $LIBS sigmod13.modified.SI_Join_Modified $inputfile_one $inputfile_two $rulefile
 	{ time java -Xmx8G -Xms4G -cp $LIBS sigmod13.modified.SI_Join_Modified $inputfile_one $inputfile_two $rulefile rslt_sijoin.txt > $dir"/"logSIJoin; }
+	date
 fi
 
 #JoinMin
 if [[ $RUN_JoinMin == "True" ]];
 then
+	date
 	./joinMin.sh $inputfile_one $inputfile_two $rulefile $outputPath $dir $LIBS $project
 	echo java -Xmx8G -Xms4G -cp $LIBS mine.JoinH2GramNoIntervalTree $inputfile_one $inputfile_two $rulefile
 	{ time java -Xmx8G -Xms4G -cp $LIBS mine.JoinH2GramNoIntervalTree $inputfile_one $inputfile_two $rulefile rslt4.txt -compact -v TopDownHashSetSinglePathDS 0 > $dir"/"logJoinH2GramCompactTopDownHashSet; }
+	date
 fi
 
 #JoinMH
 if [[ $RUN_JoinMH == "True" ]];
 then
 	for j in {1..1..1}; do
+		date
 		./joinMH.sh $inputfile_one $inputfile_two $rulefile $outputPath $dir $LIBS $j $project
 
 		echo java -Xmx8G -Xms4G -cp $LIBS mine.JoinD2GramNoIntervalTree $inputfile_one $inputfile_two $rulefile rslt$j".txt" -n $j -compact -v TopDownHashSetSinglePathDS 0
 		{ time java -Xmx8G -Xms4G -cp $LIBS mine.JoinD2GramNoIntervalTree $inputfile_one $inputfile_two $rulefile rslt$j".txt" -n $j -compact -v TopDownHashSetSinglePathDS 0 > $dir"/"logJoinD2GramCompact$j"TopDownHashSet"; }
+		date
 	done
 fi
 
@@ -86,9 +96,11 @@ if [[ $RUN_JoinHybrid == "True" ]];
 then
 	samplings=( 0.01 0.03 0.1 0.3 )
 	for sampling in "${samplings[@]}"; do
+		date
 		./joinHybrid.sh $inputfile_one $inputfile_two $rulefile $outputPath $dir $LIBS $sampling $project
 
 		echo java -Xmx8G -Xms4G -cp $LIBS mine.hybrid.Hybrid2GramWithOptTheta4 $inputfile_one $inputfile_two $rulefile rslt6.txt -compact -v TopDownHashSetSinglePathDS 0 -s $sampling
 		{ time java -Xmx8G -Xms4G -cp $LIBS mine.hybrid.Hybrid2GramWithOptTheta4 $inputfile_one $inputfile_two $rulefile rslt6.txt -compact -v TopDownHashSetSinglePathDS 0 -s $sampling > $dir"/"logAOLHybrid2GramWithOptTheta4_$sampling; }
+		date
 	done
 fi
