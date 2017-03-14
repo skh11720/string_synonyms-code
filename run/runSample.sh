2929e11119d236780dc498741e244e6c4f902ef7
yjpark at banana <yjpark@kdd.snu.ac.kr>
2017-03-14 17:00:53 +0900

    scripts

--- run/runSample.sh
+++ run/runSample.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 nTokens=1000000
-SIZES=( 100 1000 10000 100000 300000 1000000 1500000 2000000 3000000 10000000 )
+IDS=( 1 2 3 4 5 6 7 8 9 10 11 )
 #SIZES=( 10000000 100000000 )
-#nRecords=100000
 nRules=200000
 nTokensInRule=30000
 avgRecLen=5
@@ -37,14 +36,12 @@ RUN_JoinHybridOpt=True
 #RUN_JoinHybridThres=True
 RUN_JoinHybridThres=False
 
-for nRecords in ${SIZES[@]};
+for nId in ${IDS[@]};
 do
-	./setSynthetic.sh $nTokens $nRecords $nRules $nTokensInRule $avgRecLen $avgLhsLen $avgRhsLen $skewZ $ratio
+	project=syn_$nId
 
-	project=syn_$nRecords
-
-	inputfile_one=data_store/current_data_one/data.txt
-	inputfile_two=data_store/current_data_two/data.txt
+	inputfile_one=/home/kddlab/wooyekim/Synonym/AOL_Sample/sample$nId
+	inputfile_two=/home/kddlab/wooyekim/Synonym/AOL_Sample/sample$nId
 	rulefile=data_store/current_rule/rule.txt
 	outputPath=output
 
