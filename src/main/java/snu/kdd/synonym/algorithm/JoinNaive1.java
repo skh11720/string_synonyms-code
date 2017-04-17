6b65a3015a641c03adb47fc13fe3a03a0d444b2b
Yoonjae Park <yjpark@kdd.snu.ac.kr>
2017-04-17 19:26:49 +0900

    debug removed

--- src/main/java/snu/kdd/synonym/algorithm/JoinNaive1.java
+++ src/main/java/snu/kdd/synonym/algorithm/JoinNaive1.java
@@ -1,7 +1,5 @@
 package snu.kdd.synonym.algorithm;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -94,66 +92,66 @@ public class JoinNaive1 extends AlgorithmTemplate {
 		long indexingTime = 0;
 
 		// DEBUG
-		try {
-			boolean debug = true;
-			BufferedWriter debug_bw = new BufferedWriter( new FileWriter( "est_debug.txt" ) );
+		// try {
+		// boolean debug = true;
+		// BufferedWriter debug_bw = new BufferedWriter( new FileWriter( "est_debug.txt" ) );
 
-			for( int i = 0; i < tableSearched.size(); ++i ) {
-				final Record recR = tableSearched.get( i );
-				final long est = recR.getEstNumRecords();
+		for( int i = 0; i < tableSearched.size(); ++i ) {
+			final Record recR = tableSearched.get( i );
+			final long est = recR.getEstNumRecords();
 
-				if( threshold != -1 && est > threshold ) {
-					// if threshold is set (!= -1), index is built selectively for supporting hybrid algorithm
+			if( threshold != -1 && est > threshold ) {
+				// if threshold is set (!= -1), index is built selectively for supporting hybrid algorithm
+				continue;
+			}
+
+			long expandStartTime = System.nanoTime();
+			final List<Record> expanded = recR.expandAll( ruletrie );
+			expandTime += System.nanoTime() - expandStartTime;
+
+			assert ( threshold == -1 || expanded.size() <= threshold );
+
+			// if( debug ) {
+			// if( expanded.size() != est ) {
+			// debug_bw.write( recR.toString() + "\n" );
+			// debug_bw.write( "expaneded: " + expanded.size() + "\n" );
+			// debug_bw.write( "estimated: " + est + "\n" );
+			// for( int x = 0; x < expanded.size(); x++ ) {
+			// debug_bw.write( expanded.get( x ).toString() + "\n" );
+			// }
+			// debug = false;
+			// }
+			// }
+
+			totalExpSize += expanded.size();
+			estimatedExpSize += est;
+
+			long indexingStartTime = System.nanoTime();
+			for( final Record exp : expanded ) {
+				ArrayList<Integer> list = rec2idx.get( exp );
+
+				if( list == null ) {
+					// new expression
+					list = new ArrayList<>( 5 );
+					rec2idx.put( exp, list );
+				}
+
+				// If current list already contains current record as the last element, skip adding
+				if( !list.isEmpty() && list.get( list.size() - 1 ) == i ) {
 					continue;
 				}
-
-				long expandStartTime = System.nanoTime();
-				final List<Record> expanded = recR.expandAll( ruletrie );
-				expandTime += System.nanoTime() - expandStartTime;
-
-				assert ( threshold == -1 || expanded.size() <= threshold );
-
-				if( debug ) {
-					if( expanded.size() != est ) {
-						debug_bw.write( recR.toString() + "\n" );
-						debug_bw.write( "expaneded: " + expanded.size() + "\n" );
-						debug_bw.write( "estimated: " + est + "\n" );
-						for( int x = 0; x < expanded.size(); x++ ) {
-							debug_bw.write( expanded.get( x ).toString() + "\n" );
-						}
-						debug = false;
-					}
-				}
-
-				totalExpSize += expanded.size();
-				estimatedExpSize += est;
-
-				long indexingStartTime = System.nanoTime();
-				for( final Record exp : expanded ) {
-					ArrayList<Integer> list = rec2idx.get( exp );
-
-					if( list == null ) {
-						// new expression
-						list = new ArrayList<>( 5 );
-						rec2idx.put( exp, list );
-					}
-
-					// If current list already contains current record as the last element, skip adding
-					if( !list.isEmpty() && list.get( list.size() - 1 ) == i ) {
-						continue;
-					}
-					list.add( i );
-					idxsize++;
-				}
-				indexingTime += System.nanoTime() - indexingStartTime;
-				++count;
+				list.add( i );
+				idxsize++;
 			}
-			debug_bw.close();
+			indexingTime += System.nanoTime() - indexingStartTime;
+			++count;
 		}
-		catch( Exception e ) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		// debug_bw.close();
+		// }
+		// catch( Exception e ) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
 		// ((WYK_HashMap<Record, ArrayList<Integer>>) rec2idx).printStat();
 		final long duration = System.nanoTime() - starttime;
 		alpha = ( (double) duration ) / totalExpSize;
