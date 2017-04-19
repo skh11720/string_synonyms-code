6491e9242ea1dba4025b70a3a97a56f0b36c10db
Yoonjae Park <yjpark@kdd.snu.ac.kr>
2017-04-19 18:12:49 +0900

    debug

--- src/main/java/snu/kdd/synonym/algorithm/JoinNaive1.java
+++ src/main/java/snu/kdd/synonym/algorithm/JoinNaive1.java
@@ -168,8 +168,8 @@ public class JoinNaive1 extends AlgorithmTemplate {
 			}
 
 			long expandStartTime = System.nanoTime();
-			// final List<Record> expanded = recR.expandAll( ruletrie );
-			final List<Record> expanded = recR.expandAll();
+			final List<Record> expanded = recR.expandAll( ruletrie );
+			// final List<Record> expanded = recR.expandAll();
 			expandTime += System.nanoTime() - expandStartTime;
 
 			assert ( threshold == -1 || expanded.size() <= threshold );
@@ -303,8 +303,8 @@ public class JoinNaive1 extends AlgorithmTemplate {
 			}
 
 			long expandStartTime = System.nanoTime();
-			// final List<Record> expanded = recS.expandAll( ruletrie );
-			final List<Record> expanded = recS.expandAll();
+			final List<Record> expanded = recS.expandAll( ruletrie );
+			// final List<Record> expanded = recS.expandAll();
 			expandTime += System.nanoTime() - expandStartTime;
 
 			totalExpSize += expanded.size();
