de3dfa69ca3577531834c2bad11818c4a383600c
Yoonjae Park <yjpark@kdd.snu.ac.kr>
2017-04-21 20:48:59 +0900

    debug

--- src/main/java/snu/kdd/synonym/algorithm/JoinMH_QL.java
+++ src/main/java/snu/kdd/synonym/algorithm/JoinMH_QL.java
@@ -286,7 +286,7 @@ public class JoinMH_QL extends AlgorithmTemplate {
 				for( Record recR : candidates ) {
 					int compare = checker.isEqual( recR, recS );
 					if( compare >= 0 ) {
-						rslt.add( new IntegerPair( recR.getID(), recS.getID() ) );
+						rslt.add( new IntegerPair( recS.getID(), recR.getID() ) );
 					}
 				}
 				equivTime.stopQuiet();
