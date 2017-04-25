f9f7eeae795f055491bcbc8dbf5da06fbd889b7d
Yoonjae Park <yjpark@kdd.snu.ac.kr>
2017-04-25 17:46:06 +0900

    debug

--- src/main/java/tools/DEBUG.java
+++ src/main/java/tools/DEBUG.java
@@ -1,8 +1,8 @@
 package tools;
 
 public class DEBUG {
-	public static final boolean ON = true;
+	public static final boolean ON = false;
 
 	public static final boolean JoinMinIndexOn = false;
-	public static final boolean JoinMinJoinOn = true;
+	public static final boolean JoinMinJoinOn = false;
 }
