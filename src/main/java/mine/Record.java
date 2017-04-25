34f5d0efe8f787eb0bdfbefd8ccf1d00a2060ba4
Yoonjae Park <yjpark@kdd.snu.ac.kr>
2017-04-25 15:43:31 +0900

    debug

--- src/main/java/mine/Record.java
+++ src/main/java/mine/Record.java
@@ -567,7 +567,7 @@ public class Record implements Comparable<Record>, RecordInterface, RecordInterf
 
 			// WYK_HashSet.DEBUG = true;
 			// System.out.println( "Add " + pQGram.size() );
-			Set<QGram> sQGram = new WYK_HashSet<QGram>( pQGram.size() * 3 + 2 );
+			Set<QGram> sQGram = new WYK_HashSet<QGram>( pQGram.size() * 2 + 2 );
 
 			for( QGram qgram : pQGram ) {
 				sQGram.add( qgram );
