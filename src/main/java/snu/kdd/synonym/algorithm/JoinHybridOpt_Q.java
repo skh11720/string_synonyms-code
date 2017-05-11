5c249eaf85f708ffa8b527af7f7ae83d5edee248
Yoonjae Park <yjpark@kdd.snu.ac.kr>
2017-05-11 12:36:14 +0900

    debug

--- src/main/java/snu/kdd/synonym/algorithm/JoinHybridOpt_Q.java
+++ src/main/java/snu/kdd/synonym/algorithm/JoinHybridOpt_Q.java
@@ -485,8 +485,8 @@ public class JoinHybridOpt_Q extends AlgorithmTemplate {
 			stat.add( "Auto_Best_Threshold", bestThreshold );
 		}
 
-		if( bestThreshold > Integer.MAX_VALUE ) {
-			return Integer.MAX_VALUE;
+		if( bestThreshold > 10000 ) {
+			return 10000;
 		}
 
 		return (int) bestThreshold;
