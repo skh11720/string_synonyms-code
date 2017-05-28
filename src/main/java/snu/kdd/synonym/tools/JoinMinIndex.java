61ada0ea47826ba6c59d8e74fe4b7a8bdc4746eb
Yoonjae-Home <yjpark@kdd.snu.ac.kr>
2017-05-28 23:51:32 +0900

    debug

--- src/main/java/snu/kdd/synonym/tools/JoinMinIndex.java
+++ src/main/java/snu/kdd/synonym/tools/JoinMinIndex.java
@@ -508,7 +508,7 @@ public class JoinMinIndex {
 		long joinStartTime = 0;
 		long qgramCount = 0;
 
-		System.out.println( "recS " + recS );
+		// System.out.println( "recS " + recS );
 
 		boolean isUpperRecord = recS.getEstNumRecords() > threshold;
 
