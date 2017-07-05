18254c2dfa9a035b3f12e2672802c2f1b659d303
Yoonjae Park <yjpark@kdd.snu.ac.kr>
2017-07-05 16:23:57 +0900

    debug

--- revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/gui/GuiMain.java
+++ revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/gui/GuiMain.java
@@ -168,7 +168,7 @@ public class GuiMain extends JFrame {
 			}
 			else if( command.equals( "JoinMH" ) ) {
 				arg = arg + " -algorithm JoinMH -additional";
-				additional = "-K 2 -qSize 2";
+				additional = "-K 2 -qSize 1";
 			}
 			else if( command.equals( "JoinDebug" ) ) {
 				// String sample = sampleTextField.getText();
