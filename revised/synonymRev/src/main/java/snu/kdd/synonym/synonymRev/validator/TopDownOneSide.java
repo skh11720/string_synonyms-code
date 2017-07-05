c459fd9d9bb42886158da31ab0e65c4a2673f073
Yoonjae Park <yjpark@kdd.snu.ac.kr>
2017-07-05 20:11:04 +0900

    debug

--- revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/validator/TopDownOneSide.java
+++ revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/validator/TopDownOneSide.java
@@ -47,6 +47,14 @@ public class TopDownOneSide extends Validator {
 
 		Rule[] rules = x.getSuffixApplicableRules( xIdx );
 
+		// TODO: DEBUG
+		if( rules == null ) {
+			System.out.println( x );
+			System.out.println( xIdx );
+			System.out.println( y );
+			System.out.println( yIdx );
+		}
+
 		for( int ridx = 0; ridx < rules.length; ++ridx ) {
 			Rule rule = rules[ ridx ];
 
