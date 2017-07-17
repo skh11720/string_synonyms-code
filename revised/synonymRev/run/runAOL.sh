7c941a7c2ea38726f656b1521a5b0044273e7764
yjpark on tangerine <yjpark@kdd.snu.ac.kr>
2017-07-17 18:57:44 +0900

    run aol

--- revised/synonymRev/run/runAOL.sh
+++ revised/synonymRev/run/runAOL.sh
@@ -7,9 +7,9 @@ dir=logs
 #SIZES=( 1000000 )
 #SIZES=( 1000000 )
 #SIZES=( 1500000 2000000 2500000 )
-SIZES=( 30000 )
-#SIZES=( 1000000 )
-#SIZES=( 1000 3000 10000 30000 100000 )
+#SIZES=( 30000 )
+SIZES=( 100000 )
+#SIZES=( 1000 3000 10000 )
 #SIZES=( 1000 3000 10000 30000 100000 300000 1000000 3000000 5000000 )
 #SIZES=( 1000 3000 10000 30000 100000 )
 
@@ -23,8 +23,8 @@ RUN_Naive=False
 #RUN_NaiveSP=True
 RUN_NaiveSP=False
 
-RUN_JoinMHSP=True
-#RUN_JoinMHSP=False
+#RUN_JoinMHSP=True
+RUN_JoinMHSP=False
 
 #RUN_JoinMin=True
 RUN_JoinMin=False
@@ -38,8 +38,8 @@ RUN_JoinHybridOpt=False
 #RUN_JoinHybridThres=True
 RUN_JoinHybridThres=False
 
-RUN_JoinBK=True
-#RUN_JoinBK=False
+#RUN_JoinBK=True
+RUN_JoinBK=False
 
 RUN_JoinBKSP=True
 #RUN_JoinBKSP=False
