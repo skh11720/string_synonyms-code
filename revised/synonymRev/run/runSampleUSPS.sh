a48b1ddd0d016b500fde2ff7f0fa2b6e8ff1bc4f
yjpark at banana <yjpark@kdd.snu.ac.kr>
2017-07-18 23:02:47 +0900

    scripts

--- revised/synonymRev/run/runSampleUSPS.sh
+++ revised/synonymRev/run/runSampleUSPS.sh
@@ -38,8 +38,8 @@ RUN_JoinBKSP=True
 RUN_DEBUG=False
 
 #IDS=( 8 )
-#IDS=( 10 11 )
-IDS=( 1 2 3 4 5 6 7 8 9 )
+IDS=( 10 11 )
+#IDS=( 1 2 3 4 5 6 7 8 9 )
 #IDS=( 6 7 8 9 10 11 )
 #IDS=( 1 2 3 4 5 6 7 8 9 10 11 )
 for nId in ${IDS[@]};
