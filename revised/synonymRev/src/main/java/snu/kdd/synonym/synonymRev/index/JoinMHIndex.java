ec9480178fadfbf45e0e3ea4aa3c65394a809984
Yoonjae Park <yjpark@kdd.snu.ac.kr>
2017-07-28 14:57:40 +0900

    debug

--- revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/index/JoinMHIndex.java
+++ revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/index/JoinMHIndex.java
@@ -377,7 +377,8 @@ public class JoinMHIndex {
 			long executionTime = System.currentTimeMillis() - startTime;
 
 			if( candidates.size() > 100 ) {
-				System.out.println( recS.getID() + " compared " + candidates.size() );
+				System.out.println( recS.getID() + " compared " );
+				System.out.println(  candidates.size() );
 			}
 
 			// if( executionTime > 2 ) {
