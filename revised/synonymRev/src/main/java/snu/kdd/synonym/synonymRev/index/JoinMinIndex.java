8564bdb3f075736a9be7b0c6bc3a99caf85540ec
Yoonjae Park <yjpark@kdd.snu.ac.kr>
2017-08-02 16:44:17 +0900

    debug

--- revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/index/JoinMinIndex.java
+++ revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/index/JoinMinIndex.java
@@ -668,7 +668,7 @@ public class JoinMinIndex {
 			for( QGram qgram : availableQGrams.get( i ) ) {
 				if( DEBUG.PrintJoinMinJoinON ) {
 					try {
-						bw.write( "q :" + qgramRange.qgram + " " + i );
+						bw.write( "q :" + qgram + " " + i );
 					}
 					catch( IOException e1 ) {
 						e1.printStackTrace();
