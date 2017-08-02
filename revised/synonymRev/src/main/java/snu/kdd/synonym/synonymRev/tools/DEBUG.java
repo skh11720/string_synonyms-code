4dbd444fee996ea118fc880acdb70209e2047fec
Yoonjae Park <yjpark@kdd.snu.ac.kr>
2017-08-02 16:30:37 +0900

    debug

--- revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/tools/DEBUG.java
+++ revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/tools/DEBUG.java
@@ -15,7 +15,7 @@ public class DEBUG {
 
 	public static final boolean PrintNaiveIndexON = false;
 	public static final boolean PrintJoinMinIndexON = true;
-	public static final boolean PrintJoinMinJoinON = false;
+	public static final boolean PrintJoinMinJoinON = true;
 
 	public static final boolean ValidateON = false;
 	public static final boolean SampleStatON = false;
