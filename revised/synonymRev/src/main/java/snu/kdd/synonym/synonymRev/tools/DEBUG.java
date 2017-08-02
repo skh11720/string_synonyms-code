8335dfca88aeedfeb08ddc1b91d0c098840a4e9d
Yoonjae Park <yjpark@kdd.snu.ac.kr>
2017-08-02 15:56:38 +0900

    debug

--- revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/tools/DEBUG.java
+++ revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/tools/DEBUG.java
@@ -20,7 +20,7 @@ public class DEBUG {
 	public static final boolean ValidateON = false;
 	public static final boolean SampleStatON = false;
 
-	public static final boolean printSelfJoinON = false;
+	public static final boolean printSelfJoinON = true;
 
 	public static final boolean ToLowerON = true;
 
