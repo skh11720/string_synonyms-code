66073d869b2917ac61524e0475a0bed11fb4bec8
Yoonjae Park <yjpark@kdd.snu.ac.kr>
2017-08-08 14:23:09 +0900

    debug

--- revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/algorithm/misc/EstimationTest.java
+++ revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/algorithm/misc/EstimationTest.java
@@ -10,6 +10,9 @@ import snu.kdd.synonym.synonymRev.algorithm.AlgorithmTemplate;
 import snu.kdd.synonym.synonymRev.data.Query;
 import snu.kdd.synonym.synonymRev.data.Record;
 import snu.kdd.synonym.synonymRev.estimation.SampleEstimate;
+import snu.kdd.synonym.synonymRev.index.JoinMinIndex;
+import snu.kdd.synonym.synonymRev.index.NaiveIndex;
+import snu.kdd.synonym.synonymRev.tools.DEBUG;
 import snu.kdd.synonym.synonymRev.tools.IntegerPair;
 import snu.kdd.synonym.synonymRev.tools.Param;
 import snu.kdd.synonym.synonymRev.tools.StatContainer;
@@ -23,9 +26,6 @@ public class EstimationTest extends AlgorithmTemplate {
 		super( query, stat );
 	}
 
-	private long maxSearchedEstNumRecords = 0;
-	private long maxIndexedEstNumRecords = 0;
-
 	public Validator checker;
 	SampleEstimate estimate;
 	private int qSize = 0;
@@ -33,6 +33,12 @@ public class EstimationTest extends AlgorithmTemplate {
 	private double sampleRatio = 0;
 	private int joinThreshold = 1;
 	private boolean joinMinRequired = true;
+
+	NaiveIndex naiveIndex;
+	JoinMinIndex joinMinIdx;
+
+	private long maxSearchedEstNumRecords = 0;
+	private long maxIndexedEstNumRecords = 0;
 
 	@Override
 	public void preprocess() {
@@ -58,16 +64,6 @@ public class EstimationTest extends AlgorithmTemplate {
 	}
 
 	@Override
-	public String getName() {
-		return "EstimationTest";
-	}
-
-	@Override
-	public String getVersion() {
-		return "1.0";
-	}
-
-	@Override
 	public void run( Query query, String[] args ) throws IOException, ParseException {
 		Param params = Param.parseArgs( args, stat, query );
 		// Setup parameters
@@ -89,10 +85,25 @@ public class EstimationTest extends AlgorithmTemplate {
 		stat.addMemory( "Mem_4_Joined" );
 
 		stepTime.resetAndStart( "Result_4_Write_Time" );
-
+		writeResult( rslt );
 		stepTime.stopAndAdd( stat );
 	}
 
+	private void buildJoinMinIndex() {
+		// Build an index
+		joinMinIdx = new JoinMinIndex( indexK, qSize, stat, query, true );
+	}
+
+	private void buildNaiveIndex() {
+		naiveIndex = NaiveIndex.buildIndex( joinThreshold / 2, stat, joinThreshold, false, query );
+	}
+
+	/**
+	 * Although this implementation is not efficient, we did like this to measure
+	 * the execution time of each part more accurate.
+	 *
+	 * @return
+	 */
 	private ArrayList<IntegerPair> join() {
 
 		StopWatch buildTime = StopWatch.getWatchStarted( "Result_3_1_Index_Building_Time" );
@@ -106,12 +117,123 @@ public class EstimationTest extends AlgorithmTemplate {
 		}
 
 		Util.printLog( "Selected Threshold: " + joinThreshold );
-		return null;
+
+		StopWatch stepTime = StopWatch.getWatchStarted( "Result_7_0_JoinMin_Index_Build_Time" );
+
+		buildTime.start();
+		if( joinMinRequired ) {
+			buildJoinMinIndex();
+		}
+		int joinMinResultSize = 0;
+		if( DEBUG.JoinMinNaiveON ) {
+			if( joinMinRequired ) {
+				stat.add( "Const_Gamma_Actual", String.format( "%.2f", joinMinIdx.gamma ) );
+				stat.add( "Const_Gamma_SearchedSigCount_Actual", joinMinIdx.searchedTotalSigCount );
+				stat.add( "Const_Gamma_CountTime_Actual", String.format( "%.2f", joinMinIdx.countTime ) );
+
+				stat.add( "Const_Delta_Actual", String.format( "%.2f", joinMinIdx.delta ) );
+				stat.add( "Const_Delta_IndexedSigCount_Actual", joinMinIdx.indexedTotalSigCount );
+				stat.add( "Const_Delta_IndexTime_Actual", String.format( "%.2f", joinMinIdx.indexTime ) );
+			}
+			stepTime.stopAndAdd( stat );
+			stepTime.resetAndStart( "Result_7_1_SearchEquiv_JoinMin_Time" );
+		}
+		buildTime.stopQuiet();
+		StopWatch joinTime = StopWatch.getWatchStarted( "Result_3_2_Join_Time" );
+		ArrayList<IntegerPair> rslt = new ArrayList<IntegerPair>();
+		long joinstart = System.nanoTime();
+		if( joinMinRequired ) {
+			if( query.oneSideJoin ) {
+				for( Record s : query.searchedSet.get() ) {
+					// System.out.println( "test " + s + " " + s.getEstNumRecords() );
+					if( s.getEstNumTransformed() > joinThreshold ) {
+						joinMinIdx.joinRecordMaxKThres( indexK, s, rslt, true, null, checker, joinThreshold, query.oneSideJoin );
+					}
+				}
+			}
+			else {
+				for( Record s : query.searchedSet.get() ) {
+					joinMinIdx.joinRecordMaxKThres( indexK, s, rslt, true, null, checker, joinThreshold, query.oneSideJoin );
+				}
+			}
+
+			joinMinResultSize = rslt.size();
+			stat.add( "Join_Min_Result", joinMinResultSize );
+			stat.add( "Stat_Equiv_Comparison", joinMinIdx.equivComparisons );
+		}
+		double joinminJointime = System.nanoTime() - joinstart;
+		joinTime.stopQuiet();
+
+		if( DEBUG.JoinMinNaiveON ) {
+			Util.printLog( "After JoinMin Result: " + rslt.size() );
+			stat.add( "Const_Epsilon_JoinTime_Actual", String.format( "%.2f", joinminJointime ) );
+			if( joinMinRequired ) {
+				stat.add( "Const_Epsilon_Predict_Actual", joinMinIdx.predictCount );
+				stat.add( "Const_Epsilon_Actual", String.format( "%.2f", joinminJointime / joinMinIdx.predictCount ) );
+
+				stat.add( "Const_EpsilonPrime_Actual", String.format( "%.2f", joinminJointime / joinMinIdx.comparisonCount ) );
+				stat.add( "Const_EpsilonPrime_Comparison_Actual", joinMinIdx.comparisonCount );
+			}
+			stepTime.stopAndAdd( stat );
+			stepTime.resetAndStart( "Result_7_2_Naive Index Building Time" );
+		}
+
+		buildTime.start();
+		buildNaiveIndex();
+		buildTime.stopAndAdd( stat );
+
+		if( DEBUG.JoinMinNaiveON ) {
+			stat.add( "Const_Alpha_Actual", String.format( "%.2f", naiveIndex.alpha ) );
+			stat.add( "Const_Alpha_IndexTime_Actual", String.format( "%.2f", naiveIndex.indexTime ) );
+			stat.add( "Const_Alpha_ExpLength_Actual", String.format( "%.2f", naiveIndex.totalExpLength ) );
+
+			stepTime.stopAndAdd( stat );
+			stepTime.resetAndStart( "Result_7_3_SearchEquiv Naive Time" );
+		}
+
+		joinTime.start();
+		@SuppressWarnings( "unused" )
+		int naiveSearch = 0;
+		long starttime = System.nanoTime();
+		for( Record s : query.searchedSet.get() ) {
+			if( s.getEstNumTransformed() > joinThreshold ) {
+				continue;
+			}
+			else {
+				naiveIndex.joinOneRecord( s, rslt );
+				naiveSearch++;
+			}
+		}
+		double joinNanoTime = System.nanoTime() - starttime;
+
+		stat.add( "Join_Naive_Result", rslt.size() - joinMinResultSize );
+		joinTime.stopAndAdd( stat );
+
+		if( DEBUG.JoinMinNaiveON ) {
+			stat.add( "Const_Beta_Actual", String.format( "%.2f", joinNanoTime / naiveIndex.totalExp ) );
+			stat.add( "Const_Beta_JoinTime_Actual", String.format( "%.2f", joinTime ) );
+			stat.add( "Const_Beta_TotalExp_Actual", String.format( "%.2f", naiveIndex.totalExp ) );
+
+			stat.add( "Stat_Naive search count", naiveSearch );
+			stepTime.stopAndAdd( stat );
+		}
+		buildTime.stopAndAdd( stat );
+		return rslt;
 	}
 
 	private void findConstants( double sampleratio ) {
 		// Sample
 		estimate = new SampleEstimate( query, sampleratio, query.selfJoin );
 		estimate.estimateWithSample( stat, checker, indexK, qSize );
+	}
+
+	@Override
+	public String getName() {
+		return "EstimationTest";
+	}
+
+	@Override
+	public String getVersion() {
+		return "1.0";
 	}
 }
