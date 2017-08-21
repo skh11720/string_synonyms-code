f23f7652253a95be7142df7d358af66560fb643e
hadoop <hadoop@cherry41.cherry.kdd.snu.ac.kr>
2017-08-21 18:32:34 +0900

    script

--- revised/synonymRev/run/runSPROT.sh
+++ revised/synonymRev/run/runSPROT.sh
@@ -12,8 +12,8 @@ dir=logs
 #SIZES=( 158489 )
 #SIZES=( 15848 )
 #SIZES=( 10000 15848 25118 39810 63095 100000 )
-SIZES=( 10000 15848 25118 39810 46415 54116 63095 66408 69895 73564 85770 100000 158489 251188 466158 )
-#SIZES=( 10000 15848 25118 39810 63095 100000 158489 251188 466158 )
+#SIZES=( 10000 15848 25118 39810 46415 54116 63095 66408 69895 73564 85770 100000 158489 251188 466158 )
+SIZES=( 10000 15848 25118 39810 63095 100000 158489 251188 466158 )
 #SIZES=( 10000 15848 25118 39810 63095 100000 158489 251188 466158 )
 
 oneSide=$1
@@ -32,8 +32,8 @@ RUN_JoinMHSP=False
 RUN_JoinMin=True
 #RUN_JoinMin=False
 
-#RUN_JoinMH=True
-RUN_JoinMH=False
+RUN_JoinMH=True
+#RUN_JoinMH=False
 
 #RUN_JoinMinNaive=True
 RUN_JoinMinNaive=False
@@ -41,8 +41,17 @@ RUN_JoinMinNaive=False
 #RUN_JoinHybridThres=True
 RUN_JoinHybridThres=False
 
-RUN_JoinBK=True
-#RUN_JoinBK=False
+#RUN_JoinMHNaive=True
+RUN_JoinMHNaive=False
+
+#RUN_JoinMHNaiveThres=True
+RUN_JoinMHNaiveThres=False
+
+RUN_JoinHybridAll=True
+#RUN_JoinHybridAll=False
+
+#RUN_JoinBK=True
+RUN_JoinBK=False
 
 #RUN_JoinBKSP=True
 RUN_JoinBKSP=False
@@ -60,7 +69,7 @@ do
 	rulefile=data_store/sprot/rule.txt
 	outputPath=output
 
-	./runAlgorithms.sh $project $inputfile_one $inputfile_two $rulefile $outputPath $dir $RUN_Naive $RUN_JoinNaiveSP $RUN_JoinMHSP $RUN_JoinMin $RUN_JoinMH $RUN_JoinMinNaive $RUN_JoinHybridThres $RUN_JoinBK $RUN_JoinBKSP $RUN_DEBUG $oneSide $UPLOAD
+	./runAlgorithms.sh $project $inputfile_one $inputfile_two $rulefile $outputPath $dir $RUN_Naive $RUN_JoinNaiveSP $RUN_JoinMHSP $RUN_JoinMin $RUN_JoinMH $RUN_JoinMinNaive $RUN_JoinHybridThres $RUN_JoinMHNaive $RUN_JoinMHNaiveThres $RUN_JoinHybridAll $RUN_JoinBK $RUN_JoinBKSP $RUN_DEBUG $oneSide $UPLOAD
 
 	if [[ $UPLOAD == "True" ]];
 	then
