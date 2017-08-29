953a6a2e8ae583b15fb2f85043ff907fabf16ef7
Yoonjae Park <yjpark@kdd.snu.ac.kr>
2017-08-29 14:05:13 +0900

    debug

---
+++ revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/validator/Naive.java
@@ -0,0 +1,58 @@
+package snu.kdd.synonym.synonymRev.validator;
+
+import java.util.HashSet;
+import java.util.List;
+
+import snu.kdd.synonym.synonymRev.data.Record;
+
+public class Naive extends Validator {
+
+	public int isEqual( Record x, Record y ) {
+		// If there is no pre-expanded records, do expand
+		if( x.equals( y ) ) {
+			return 1;
+		}
+
+		List<Record> expandedX = x.expandAll();
+
+		List<Record> expandedY = y.expandAll();
+
+		HashSet<Record> hashSet = new HashSet<>();
+
+		boolean bigX = true;
+		if( expandedX.size() < expandedY.size() ) {
+			bigX = false;
+		}
+		if( bigX ) {
+			for( Record r : expandedX ) {
+				hashSet.add( r );
+			}
+		}
+		else {
+			for( Record r : expandedY ) {
+				hashSet.add( r );
+			}
+		}
+
+		if( bigX ) {
+			for( Record yPrime : expandedY ) {
+				if( hashSet.contains( yPrime ) ) {
+					return 1;
+				}
+			}
+		}
+		else {
+			for( Record xPrime : expandedX ) {
+				if( hashSet.contains( xPrime ) ) {
+					return 1;
+				}
+			}
+		}
+
+		return -1;
+	}
+
+	public String getName() {
+		return "NaiveOneSide";
+	}
+}
