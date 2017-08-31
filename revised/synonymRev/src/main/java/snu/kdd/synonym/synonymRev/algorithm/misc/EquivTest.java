d4eb70cee1ee4d6c26b8e2bab4d18782f03fd476
Yoonjae Park <yjpark@kdd.snu.ac.kr>
2017-08-31 19:47:16 +0900

    debug

--- revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/algorithm/misc/EquivTest.java
+++ revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/algorithm/misc/EquivTest.java
@@ -52,7 +52,7 @@ public class EquivTest extends AlgorithmTemplate {
 		ArrayList<Record> sampleX = new ArrayList<>();
 		Random rn = new Random( seed );
 		for( Record x : query.searchedSet.get() ) {
-			if( x.getEstNumTransformed() > 1500 ) {
+			if( x.getEstNumTransformed() > 5000 ) {
 				continue;
 			}
 
@@ -63,7 +63,7 @@ public class EquivTest extends AlgorithmTemplate {
 
 		ArrayList<Record> sampleY = new ArrayList<>();
 		for( Record y : query.searchedSet.get() ) {
-			if( y.getEstNumTransformed() > 1500 ) {
+			if( y.getEstNumTransformed() > 5000 ) {
 				continue;
 			}
 
