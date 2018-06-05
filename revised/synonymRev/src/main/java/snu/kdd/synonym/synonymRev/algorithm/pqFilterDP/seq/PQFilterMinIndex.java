d7612d31a5439081b6416815a118e86bf2375665
ghsong <ghsong@kdd.snu.ac.kr>
2018-06-05 17:22:13 +0900

    Improve JoinMinDP
    reduce the size of candidatepqgrams
    by considering pqgrams in the index

--- revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/algorithm/pqFilterDP/seq/PQFilterMinIndex.java
+++ revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/algorithm/pqFilterDP/seq/PQFilterMinIndex.java
@@ -1,36 +1,13 @@
 package snu.kdd.synonym.synonymRev.algorithm.pqFilterDP.seq;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
-import it.unimi.dsi.fastutil.objects.Object2IntMap;
-import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
-import it.unimi.dsi.fastutil.objects.ObjectIterator;
-import snu.kdd.synonym.synonymRev.algorithm.misc.EstimationTest;
 import snu.kdd.synonym.synonymRev.data.Query;
 import snu.kdd.synonym.synonymRev.data.Record;
 import snu.kdd.synonym.synonymRev.index.JoinMinIndex;
-import snu.kdd.synonym.synonymRev.tools.DEBUG;
-import snu.kdd.synonym.synonymRev.tools.Histogram;
-import snu.kdd.synonym.synonymRev.tools.IntegerPair;
-import snu.kdd.synonym.synonymRev.tools.MinPositionQueue;
-import snu.kdd.synonym.synonymRev.tools.MinPositionQueue.MinPosition;
 import snu.kdd.synonym.synonymRev.tools.QGram;
 import snu.kdd.synonym.synonymRev.tools.StatContainer;
-import snu.kdd.synonym.synonymRev.tools.StaticFunctions;
-import snu.kdd.synonym.synonymRev.tools.StopWatch;
-import snu.kdd.synonym.synonymRev.tools.Util;
-import snu.kdd.synonym.synonymRev.tools.WYK_HashMap;
-import snu.kdd.synonym.synonymRev.tools.WYK_HashSet;
-import snu.kdd.synonym.synonymRev.validator.Validator;
 
 public class PQFilterMinIndex extends JoinMinIndex {
 
@@ -45,8 +22,10 @@ public class PQFilterMinIndex extends JoinMinIndex {
 		List<List<QGram>> candidatePQGrams = new ArrayList<List<QGram>>();
 		PosQGramFilterDP filter = new PosQGramFilterDP(rec, qSize);
 		for ( int k=0; k<availableQGrams.size(); ++k ) {
+			if ( k >= idx.size() ) continue;
 			List<QGram> qgrams = new ArrayList<QGram>();
 			for ( QGram qgram : availableQGrams.get( k ) ) {
+				if ( !idx.get( k ).containsKey( qgram ) ) continue;
 				if ( filter.existence( qgram, k ) ) qgrams.add( qgram );
 			}
 			candidatePQGrams.add( qgrams );
