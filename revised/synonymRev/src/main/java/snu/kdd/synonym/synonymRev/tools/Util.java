adba6d166f6fa47dd460b4e67d7f90cd5df44a16
ghsong <ghsong@kdd.snu.ac.kr>
2018-06-15 18:53:07 +0900

    NaiveOneSideDelta

--- revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/tools/Util.java
+++ revised/synonymRev/src/main/java/snu/kdd/synonym/synonymRev/tools/Util.java
@@ -3,6 +3,7 @@ package snu.kdd.synonym.synonymRev.tools;
 import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -10,6 +11,8 @@ import java.util.Set;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
 
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectArrayFIFOQueue;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 
 public class Util {
@@ -181,4 +184,44 @@ public class Util {
 		}
 		return qgramPrefixList;
 	}
+
+	public static List<IntArrayList> getCombinations( int n, int k ) {
+		/*
+		 * Return all combinations of n choose k.
+		 */
+		List<IntArrayList> combList = new ObjectArrayList<IntArrayList>();
+
+		ObjectArrayFIFOQueue<IntArrayList> stack_x_errors = new ObjectArrayFIFOQueue<IntArrayList>();
+		stack_x_errors.enqueue( new IntArrayList() );
+		
+		while ( !stack_x_errors.isEmpty() ) {
+			IntArrayList comb = stack_x_errors.dequeue();
+			if ( comb.size() == k ) combList.add( comb );
+			else {
+				int max = comb.size() > 0 ? Collections.max( comb ) : -1;
+				for ( int i=max+1; i<n; ++i ) {
+					if ( !comb.contains( i )) {
+						IntArrayList comb2 = new IntArrayList( comb );
+						comb2.add( i );
+						stack_x_errors.enqueue( comb2 );
+					}
+				}
+			}
+		}
+		return combList;
+	}
+
+	public static int[] getSubsequence( int[] arr, IntArrayList idxList ) {
+		/*
+		 * Return the subsequence of arr with indexes in idxList.
+		 */
+		if ( idxList.size() == 0 ) return null;
+		else {
+			int[] out = new int[idxList.size()];
+			int i = 0;
+			for ( int idx : idxList ) out[i++] = arr[idx];
+			return out;
+		}
+	}
+
 }
