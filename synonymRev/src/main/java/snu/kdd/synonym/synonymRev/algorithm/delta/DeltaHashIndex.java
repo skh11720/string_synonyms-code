c6a62bbdbed8ce59b2e7365d1a80ae9714f744b2
ghsong (GHSONG-PC) <ghsong@kdd.snu.ac.kr>
2018-11-15 18:59:24 +0900

    bug fix

--- synonymRev/src/main/java/snu/kdd/synonym/synonymRev/algorithm/delta/DeltaHashIndex.java
+++ synonymRev/src/main/java/snu/kdd/synonym/synonymRev/algorithm/delta/DeltaHashIndex.java
@@ -1,10 +1,11 @@
 package snu.kdd.synonym.synonymRev.algorithm.delta;
 
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Set;
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
-import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import snu.kdd.synonym.synonymRev.algorithm.AlgorithmTemplate;
@@ -18,9 +19,9 @@ import snu.kdd.synonym.synonymRev.validator.Validator;
 
 public class DeltaHashIndex extends AbstractIndex {
 
-	protected final ObjectArrayList<Object2ObjectOpenHashMap<int[], List<Record>>> idx; 
+	protected final ObjectArrayList<Int2ObjectOpenHashMap<List<Record>>> idx; 
 	/*
-	 * idx is a list of hash tables. idx[d] is a hash table for d'-variants of strings ( 0 <= d' <= d)in the indexedSet.
+	 * idx is a list of hash tables. idx[d] is a hash table for d-variants of strings in the indexedSet.
 	 * The range of d is 0~deltaMax.
 	 */
 	protected final int deltaMax;
@@ -31,48 +32,69 @@ public class DeltaHashIndex extends AbstractIndex {
 		this.deltaMax = deltaMax;
 		this.isSelfJoin = query.selfJoin;
 		idx = new ObjectArrayList<>();
-		for ( int d=0; d<=deltaMax; ++d ) idx.add(new Object2ObjectOpenHashMap<>());
+		for ( int d=0; d<=deltaMax; ++d ) idx.add(new Int2ObjectOpenHashMap<>());
 		int size = 0;
 		
 		for ( Record recT : query.indexedSet.recordList ) {
-			int[] arrT = recT.getTokensArray();
-			for ( int d=0; d<=deltaMax; ++d ) {
-				List<IntArrayList> combList = Util.getCombinationsAll( recT.size(), d );
-				for ( IntArrayList idxList : combList ) {
-					int[] key = Util.getSubsequence( arrT, idxList );
-					if ( !idx.get(d).containsKey(key) ) idx.get(d).put(key, new ObjectArrayList<Record>() );
-					idx.get(d).get(key).add(recT);
-					++size;
-				}
+			List<IntArrayList> combList = Util.getCombinationsAll( recT.size(), deltaMax ); // indexes whose elements will be deleted
+			for ( IntArrayList idxList : combList ) {
+				int key = getKey(recT.getTokensArray(), idxList);
+				int d = idxList.size();
+				if ( !idx.get(d).containsKey(key) ) idx.get(d).put(key, new ObjectArrayList<Record>() );
+				idx.get(d).get(key).add(recT);
+//				System.out.println(d+", "+key+", "+recT);
+				++size;
 			}
 		}
 		
-		stat.add("Stat_Table_Size", size );
+		// check hash collision
+		int n_list = 0;
+		for ( Int2ObjectOpenHashMap<List<Record>> map : idx ) n_list += map.size();
+
+		stat.add("Stat_Index_Size", size );
+		stat.add("Stat_Avg_List_Length", (double)size/n_list);
 	}
 
 	@Override
 	protected void joinOneRecord( Record recS, Set<IntegerPair> rslt, Validator checker ) {
 		Set<Record> matched = new ObjectOpenHashSet<>();
 		for ( Record exp : recS.expandAll() ) {
+//			System.out.println("exp: "+exp);
 			Set<Record> candidates = new ObjectOpenHashSet<>();
-			int[] arrExp = exp.getTokensArray();
-			for ( int d=0; d<=deltaMax; ++d ) {
-				List<IntArrayList> combList = Util.getCombinations( exp.size(), d );
-				for ( IntArrayList idxList : combList ) {
-					int[] key = Util.getSubsequence( arrExp, idxList );
-					if ( idx.get(deltaMax-d).containsKey(key) ) candidates.addAll( idx.get(deltaMax-d).get(key) );
-				} // end for idxList
-			} // end for d
+			List<IntArrayList> combList = Util.getCombinationsAll( exp.size(), deltaMax );
+			for ( IntArrayList idxList : combList ) {
+				int key = getKey(exp.getTokensArray(), idxList);
+				int d_s = idxList.size();
+				for ( int d_t=0; d_t<=deltaMax; ++d_t ) {
+					if ( idx.get(d_t).containsKey(key) ) {
+						if ( d_s + d_t <= deltaMax ) {
+							for ( Record recT : idx.get(d_t).get(key) ) {
+								if ( exp.equals(recT) ) matched.add(recT);
+								else candidates.add(recT);
+							}
+						}
+						else 
+							candidates.addAll( idx.get(d_t).get(key) );
+					}
+				}
+//				System.out.println(d_s+", "+key+", "+candidates.size());
+			} // end for idxList
 
-			candidates.removeAll(matched);
+//			candidates.removeAll(matched);
 			n_verified += candidates.size();
 			for ( Record recT : candidates ) {
-				if ( isEquivalent( exp, recT ) ) {
-					AlgorithmTemplate.addSeqResult(recS, recT, rslt, isSelfJoin);
+				if ( matched.contains(recT) ) continue;
+				/*
+				 * Even though exp and recT is equivalent, we have to compute the edit distance.
+				 * e.g., ABCDE and CDEFG with deltaMax=2
+				 */
+                if ( Util.edit( exp.getTokensArray(), recT.getTokensArray(), deltaMax, 0, 0, exp.size(), recT.size() ) <= deltaMax ) {
 					matched.add(recT);
 				}
 			}
 		} // end for exp
+		
+		for ( Record recT : matched ) AlgorithmTemplate.addSeqResult(recS, recT, rslt, isSelfJoin);
 	}
 
 	@Override
@@ -80,8 +102,31 @@ public class DeltaHashIndex extends AbstractIndex {
 		stat.add("Val_Comparisons", n_verified );
 	}
 	
-	protected boolean isEquivalent( Record x, Record y ) {
-		if ( deltaMax == 0 ) return true;
-		return Util.edit( x.getTokensArray(), y.getTokensArray(), deltaMax, 0, 0, x.size(), y.size() ) <= deltaMax;
+//	protected boolean isEquivalent( Record x, Record y ) {
+//		if ( deltaMax == 0 ) return true;
+//		return Util.edit( x.getTokensArray(), y.getTokensArray(), deltaMax, 0, 0, x.size(), y.size() ) <= deltaMax;
+//	}
+	
+	protected static int getKey( int[] arr, IntArrayList idxList ) {
+		/*
+		 * Return the key value of the subsequence of arr WITHOUT indexes in idxList.
+		 * (i.e., idxList represents the positions of elements in arr to be deleted.)
+		 * This function is similar to the hash function of Record class.
+		 */
+		int key = 0;
+		int j = 0;
+		int idx = idxList.size() > 0 ? idxList.getInt(j) : -1;
+		for ( int i=0; i<arr.length; ++i ) {
+			if ( idx == i ) {
+				if ( j < idxList.size()-1 ) idx = idxList.getInt(++j);
+				else idx = -1;
+			}
+			else {
+				key = ( key << 3 ) + arr[i];
+				key %= Util.bigprime;
+			}
+		}
+//		key %= Integer.MAX_VALUE; // is this line necessary?
+		return key;
 	}
 }
