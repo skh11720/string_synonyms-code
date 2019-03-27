0de1b714ea86bde38c40ad2f315286423499ceb5
ghsong (GHSONG-PC) <ghsong@kdd.snu.ac.kr>
2019-03-27 19:04:15 +0900

    update upload.sh

--- synonymRev/run/upload.sh
+++ synonymRev/run/upload.sh
@@ -10,11 +10,13 @@ then
         echo "Downloadling uploadExperiment.py"
         mkdir uploader
         cd uploader
-        git clone ssh://yjpark@147.46.143.74/home/yjpark/repository/ExperimentUploader/
+#        git clone ssh://yjpark@147.46.143.74/home/yjpark/repository/ExperimentUploader/
+        wget -O uploadExperiment.py http://147.46.143.74:18000/projects/expUploader/
+        chmod u+x uploadExperiment.py
 else
-        cd uploader/ExperimentUploader
+        cd uploader
         #git pull
 fi
 
 cd -
-./uploader/ExperimentUploader/uploadExperiment.py exp
+./uploader/uploadExperiment.py exp
