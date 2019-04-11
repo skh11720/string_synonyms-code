1aa879ae922c03a5a41dfdf779771f79ca202b09
ghsong (GHSONG-PC) <ghsong@kdd.snu.ac.kr>
2019-04-11 18:31:03 +0900

    rename symbols

--- synonymRev/src/main/java/snu/kdd/synonym/synonymRev/algorithm/delta/JoinDeltaHybrid.java
+++ synonymRev/src/main/java/snu/kdd/synonym/synonymRev/algorithm/delta/JoinDeltaHybrid.java
@@ -22,11 +22,11 @@ public class JoinDeltaHybrid extends AbstractPosQGramBasedAlgorithm {
 	protected int nEst = 1;
 	protected int joinThreshold = 1;
 	protected boolean joinQGramRequired = true;
-	protected boolean joinMinSelected = false;
+	protected boolean joinBKSelected = false;
 
 	protected JoinDeltaNaiveIndex naiveIndex;
-	protected JoinDeltaVarBKIndex joinMinIdx = null;
-	protected JoinDeltaVarIndex joinMHIdx = null;
+	protected JoinDeltaVarBKIndex joinBKIndex = null;
+	protected JoinDeltaVarIndex joinFKIndex = null;
 
 	protected long maxSearchedEstNumRecords = 0;
 	protected long maxIndexedEstNumRecords = 0;
@@ -69,11 +69,11 @@ public class JoinDeltaHybrid extends AbstractPosQGramBasedAlgorithm {
 		StatContainer statEst = new StatContainer();
 		int[] list_thres = new int[nEst];
 		double[] list_bestTime = new double[nEst];
-		boolean[] list_minSelected= new boolean[nEst];
+		boolean[] list_bkSelected= new boolean[nEst];
 		for ( int i=0; i<nEst; ++i ) {
 			findConstants( sampleH, statEst );
 			list_thres[i] = estimate.findThetaJoinHybridAll( qSize, indexK, statEst, maxIndexedEstNumRecords, maxSearchedEstNumRecords, query.oneSideJoin );
-			list_minSelected[i] = estimate.getJoinMinSelected();
+			list_bkSelected[i] = estimate.getJoinBKSelected();
 			list_bestTime[i] = estimate.bestEstTime;
 		}
 		double mean = 0, var = 0;
@@ -86,19 +86,19 @@ public class JoinDeltaHybrid extends AbstractPosQGramBasedAlgorithm {
 			if ( list_bestTime[i] < bestTime ) {
 				bestTime = list_bestTime[i];
 				joinThreshold = list_thres[i];
-				joinMinSelected = list_minSelected[i];
+				joinBKSelected = list_bkSelected[i];
 			}
-			System.out.println( list_thres[i]+"\t"+list_bestTime[i]+"\t"+list_minSelected[i] );
+			System.out.println( list_thres[i]+"\t"+list_bestTime[i]+"\t"+list_bkSelected[i] );
 		}
 
 		Util.printLog( "Selected Threshold: " + joinThreshold );
-		Util.printLog( "JoinMinSelected: " + (joinMinSelected? "true":"false") );
+		Util.printLog( "JoinBKSelected: " + (joinBKSelected? "true":"false") );
 		stat.add( "Estimate_Threshold", joinThreshold );
 		stat.add( "Estimate_Repeat", nEst );
 		stat.add( "Estimate_Best_Time", bestTime );
 		stat.add( "Estimate_Mean_Threshold", mean );
 		stat.add( "Estimate_Var_Threshold", var );
-		stat.add( "Estimate_JoinMinSelected", joinMinSelected? "true":"false" );
+		stat.add( "Estimate_JoinBKSelected", joinBKSelected? "true":"false" );
 		estimateTime.stopAndAdd( stat );
 		
 		buildIndex();
@@ -119,9 +119,9 @@ public class JoinDeltaHybrid extends AbstractPosQGramBasedAlgorithm {
 				++skipped;
 				continue;
 			}
-			if( joinQGramRequired && s.getEstNumTransformed() > joinThreshold ) {
-				if( joinMinSelected ) joinMinIdx.joinOneRecord( s, rsltPQGram, checker );
-				else joinMHIdx.joinOneRecord( s, rsltPQGram, checker );
+			if ( joinQGramRequired && s.getEstNumTransformed() > joinThreshold ) {
+				if ( joinBKSelected ) joinBKIndex.joinOneRecord( s, rsltPQGram, checker );
+				else joinFKIndex.joinOneRecord( s, rsltPQGram, checker );
 				++pqgramSearch;
 				joinPQGramTime += System.nanoTime() - joinStartOne;
 			}
@@ -134,7 +134,7 @@ public class JoinDeltaHybrid extends AbstractPosQGramBasedAlgorithm {
 		long joinTime = System.nanoTime() - joinStart;
 
 		stat.add( "Join_Naive_Result", rsltNaive.size() );
-		stat.add( "Join_Min_Result", rsltPQGram.size() );
+		stat.add( "Join_PQGram_Result", rsltPQGram.size() );
 		stat.add( JOIN_AFTER_INDEX_TIME, joinTime/1e6 );
 		stat.add( "Result_3_2_1_Join_Naive_Time", joinNaiveTime/1e6 );
 		stat.add( "Result_3_2_2_Join_PQGram_Time", joinPQGramTime/1e6 );
@@ -144,8 +144,8 @@ public class JoinDeltaHybrid extends AbstractPosQGramBasedAlgorithm {
 		if ( joinQGramRequired ) {
 			long candQGramCountSum = 0;
 			double candQGramAvgCount = 0;
-			if ( joinMinSelected ) candQGramCountSum = joinMinIdx.algstat.candQGramCount;
-			else candQGramCountSum = joinMHIdx.algstat.candQGramCount;
+			if ( joinBKSelected ) candQGramCountSum = joinBKIndex.algstat.candQGramCount;
+			else candQGramCountSum = joinFKIndex.algstat.candQGramCount;
 			candQGramAvgCount = pqgramSearch == 0 ? 0 : 1.0 * candQGramCountSum / pqgramSearch;
 			stat.add( "Stat_CandQGram_Sum", candQGramCountSum );
 			stat.add( "Stat_CandQGram_Avg", candQGramAvgCount );
@@ -161,23 +161,23 @@ public class JoinDeltaHybrid extends AbstractPosQGramBasedAlgorithm {
 	protected void buildIndex() {
 		StopWatch buildTime = StopWatch.getWatchStarted( INDEX_BUILD_TIME );
 		if ( maxSearchedEstNumRecords <= joinThreshold ) {
-			joinQGramRequired = false; // in this case both joinmh and joinmin are not used.
+			joinQGramRequired = false; // in this case both joinFK and joinBK are not used.
 		}
 
 		if( joinQGramRequired ) {
-			if( joinMinSelected ) buildJoinMinIndex();
-			else buildJoinMHIndex();
+			if( joinBKSelected ) buildJoinBKIndex();
+			else buildJoinFKIndex();
 		}
 		buildNaiveIndex();
 		buildTime.stopAndAdd( stat );
 	}
 
-	protected void buildJoinMinIndex() {
-		joinMinIdx= new JoinDeltaVarBKIndex(query, indexK, qSize, deltaMax, distFunc, sampleB);
+	protected void buildJoinBKIndex() {
+		joinBKIndex= new JoinDeltaVarBKIndex(query, indexK, qSize, deltaMax, distFunc, sampleB);
 	}
 
-	protected void buildJoinMHIndex() {
-		joinMHIdx = new JoinDeltaVarIndex(query, indexK, qSize, deltaMax, distFunc);
+	protected void buildJoinFKIndex() {
+		joinFKIndex = new JoinDeltaVarIndex(query, indexK, qSize, deltaMax, distFunc);
 	}
 
 	protected void buildNaiveIndex() {
@@ -194,21 +194,21 @@ public class JoinDeltaHybrid extends AbstractPosQGramBasedAlgorithm {
 		this.stat.add( "Estimate_Term2_Naive", estimate.naive_term2[estimate.sampleSearchedSize-1]);
 		this.stat.add( "Estimate_Time_Naive", estimate.estTime_naive );
 
-		this.stat.add( "Estimate_Coeff1_Mh", estimate.coeff_mh_1);
-		this.stat.add( "Esitmate_Coeff2_Mh", estimate.coeff_mh_2);
-		this.stat.add( "Estimate_Coeff3_Mh", estimate.coeff_mh_3);
-		this.stat.add( "Estimate_Term1_Mh", estimate.mh_term1);
-		this.stat.add( "Estimate_Term2_Mh", estimate.mh_term2[estimate.sampleSearchedSize-1]);
-		this.stat.add( "Estimate_Term3_Mh", estimate.mh_term3[estimate.sampleSearchedSize-1]);
-		this.stat.add( "Estimate_Time_Mh", estimate.estTime_mh );
+		this.stat.add( "Estimate_Coeff1_FK", estimate.coeff_FK_1);
+		this.stat.add( "Esitmate_Coeff2_FK", estimate.coeff_FK_2);
+		this.stat.add( "Estimate_Coeff3_FK", estimate.coeff_FK_3);
+		this.stat.add( "Estimate_Term1_FK", estimate.fk_term1);
+		this.stat.add( "Estimate_Term2_FK", estimate.fk_term2[estimate.sampleSearchedSize-1]);
+		this.stat.add( "Estimate_Term3_FK", estimate.fk_term3[estimate.sampleSearchedSize-1]);
+		this.stat.add( "Estimate_Time_FK", estimate.estTime_FK );
 
-		this.stat.add( "Estimate_Coeff1_Min", estimate.coeff_min_1);
-		this.stat.add( "Estimate_Coeff2_Min", estimate.coeff_min_2);
-		this.stat.add( "Estimate_Coeff3_Min", estimate.coeff_min_3);
-		this.stat.add( "Estimate_Term1_Min", estimate.min_term1);
-		this.stat.add( "Estimate_Term2_Min", estimate.min_term2[estimate.sampleSearchedSize-1]);
-		this.stat.add( "Estimate_Term3_Min", estimate.min_term3[estimate.sampleSearchedSize-1]);
-		this.stat.add( "Estimate_Time_Min", estimate.estTime_min );
+		this.stat.add( "Estimate_Coeff1_BK", estimate.coeff_BK_1);
+		this.stat.add( "Estimate_Coeff2_BK", estimate.coeff_BK_2);
+		this.stat.add( "Estimate_Coeff3_BK", estimate.coeff_BK_3);
+		this.stat.add( "Estimate_Term1_BK", estimate.bk_term1);
+		this.stat.add( "Estimate_Term2_BK", estimate.bk_term2[estimate.sampleSearchedSize-1]);
+		this.stat.add( "Estimate_Term3_BK", estimate.bk_term3[estimate.sampleSearchedSize-1]);
+		this.stat.add( "Estimate_Time_BK", estimate.estTime_BK );
 	}
 
 	@Override
